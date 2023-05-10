978be3f23516cd12f91e5b6c94f40874ce053076
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-09 19:44:15 -0700

    seperated Pokedex and Pokecard into their own file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import pokemon from './pokemon';
-
-const Pokecard = ({name, image, type, exp}) => {
-  return (
-    <>
-      <p>{name}</p>
-      <img src={image} width={200}/>
-      <p>type: {type}</p>
-      <p>EXP : {exp}</p>
-    </>
-  )
-}
-
-const Pokedex = ({pokemon}) => {
-  return (
-    <>
-      {pokemon.map(p => (
-            <Pokecard 
-              key={p.id} 
-              name={p.name} 
-              image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`} 
-              type={p.type} 
-              exp={p.base_experience}
-            />))}
-    </>
-  )
-}
+import Pokedex from './Pokedex';
 
 const App = () => {
   return (
