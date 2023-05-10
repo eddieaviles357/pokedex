22bd177123137bc91c5e135e5bb9d58d2e56d4b5
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-09 19:58:16 -0700

    className attribute added for styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Pokecard.css';
 
 const Pokecard = ({name, image, type, exp}) => {
     return (
-      <>
-        <p>{name}</p>
+      <div className="Pokecard">
+        <p className="Pokecard-title">{name}</p>
         <img alt={name} src={image} width={200}/>
-        <p>type: {type}</p>
+        <p>Type: {type}</p>
         <p>EXP : {exp}</p>
-      </>
+      </div>
     )
   }
 
